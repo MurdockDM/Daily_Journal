6652403884250360c7ce00d46844e2c00a385dff
Dustin Murdock <dmurdock2@gmail.com>
2020-01-20 08:53:58 -0600

    added JS

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,6 +1,29 @@
 const journalEntry = {
-    dateEntered:    ,
-    concepts:   ,
-    personalEntry:  ,
-    mood:   
+    dateEntered: 01/19/2020,
+    concepts: "Javascript Objects",
+    personalEntry:"I feel somewhat comfortable with the ideas concerning Objects in Javascript. I can see how they are extremely versatile",
+    mood: "curious"   
+}
+
+
+const myJournal = []
+
+
+myJournal.push(journalEntry);
+
+
+const journalEntry_2 = {
+    dateEntered: 01/20/2020,
+    concepts: "Javascript Functions",
+    personalEntry:"So far I feel as though I have a grasp on functions based on the material we have gone over so far."
+    mood: "excited"   
+}
+
+
+
+const journalEntry_3 = {
+    dateEntered: 01/20/2020,
+    concepts: "methods",
+    personalEntry:"These were really fun to work with and learn how to use to manipulate arrays that are part of Objects",
+    mood: "happy"   
 }
