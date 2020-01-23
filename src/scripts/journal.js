71279d1faf0a037b4fdf575c2f3751dace545398
Dustin Murdock <dmurdock2@gmail.com>
2020-01-23 15:14:00 -0600

    added js to pull entries from entries.json and insert into DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,12 +1,5 @@
 
 
-
-/*
-    Purpose: To create, and return, a string template that
-    represents a single journal entry object as HTML
-
-    Arguments: journalEntry (object)
-*/
 const makeJournalEntryComponent = (journalEntry) => {
     // Create your own HTML structure for a journal entry
     return `<section class="eachEntry offset__right">
@@ -17,18 +10,19 @@ const makeJournalEntryComponent = (journalEntry) => {
 }
 
 
-const entryContainer = document.querySelector(".entryLog")
 
-/*
-    Purpose: To render all journal entries to the DOM
-
-    Arguments: entries (array of objects)
-*/
-const renderJournalEntries = (arrayOfObjects) => {
-    for (let i = 0; i < arrayOfObjects.length; i++){
-        entryContainer.innerHTML += makeJournalEntryComponent(arrayOfObjects[i])
-    }
+const renderJournalEntries = (allEntries) => {
+    const entryContainer = document.querySelector(".entryLog"); 
+    allEntries.forEach(entry => {  
+    entryContainer.innerHTML += makeJournalEntryComponent(entry);
+    })
 }
 
-// Invoke the render function
-renderJournalEntries(journalEntries);
+const entriesUrl = "http://localhost:8088/entries"
+
+fetch(entriesUrl) // Fetch from the API
+    .then(fetchedEntries => fetchedEntries.json())  // Parse as JSON
+    .then(entries => {
+        renderJournalEntries(entries)
+    })
+    
