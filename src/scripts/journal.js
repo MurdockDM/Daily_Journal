f45178387d10724a3e7a51186561a87b99b1c11c
Dustin Murdock <dmurdock2@gmail.com>
2020-01-31 14:20:21 -0600

    performed a few updates to js file structure

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,13 +1,7 @@
 import API from "./data.js"
 import renderJournalEntries from "./entriesDOM.js"
 
-/*
-    Main application logic that uses the functions and objects
-    defined in the other JavaScript files.
-
-    Change the fake variable names below to what they should be
-    to get the data and display it.
-*/
+const submitJournalEntryBtn = 
 
 API.getJournalEntries().then(renderJournalEntries)
 
